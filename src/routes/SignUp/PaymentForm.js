1feae392d7d8b5283067ebf64c593ffaf8171ad0
ase19410008 <ase19410008@wiz.ac.jp>
2022-12-13 20:59:40 +0900

    feat dashboard

---
+++ src/routes/SignUp/PaymentForm.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+
+export default function PaymentForm() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Payment method
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Grid container>
+              <Grid item xs>
+              </Grid>
+              <Grid item>
+                {/* <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link> */}
+              </Grid>
+            </Grid>
+          </Box>
+    </React.Fragment>
+  );
+}
---
+++ src/routes/routes.js
@@ -0,0 +1,43 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+
+import ErrorPage from "../error-page";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp/SignUp";
+import School from "./School/index";
+import Edit from "./School/Edit";
+import DashBoard from "./DashBoard/DashBoard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignIn />,
+    errorElement: <ErrorPage />,
+    /*loader: rootLoader,
+    children: [
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],*/
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/school",
+    element: <School />,
+  },
+  {
+    path: "/school/edit",
+    element: <Edit />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashBoard />,
+  },
+]);
+
+export default router;
